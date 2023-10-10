4a336b7975ed8ae894ecf18c76e9b839dc2ef2d9
Rupashree-S <147474468+Rupashree-S@users.noreply.github.com>
2023-10-10 11:14:56 +0530

    Add files via upload

---
+++ TypesOfVariables2.java
@@ -0,0 +1,19 @@
+package com.edu;
+class Test1{
+	static int a=30;
+	int b = 50; //instance variable
+}
+
+class Test{
+	static int a = 20;
+}
+
+public class TypesOfVariables2 {
+    static int a =10;
+	public static void main(String[] args) {
+		Test1 ob=new Test1();
+		System.out.println(a)
+		
+	}
+
+}
