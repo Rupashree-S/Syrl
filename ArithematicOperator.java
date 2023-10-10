7f0f7ee3e4ddcccbc263128ff261cd06f752d6ee
Rupashree-S <147474468+Rupashree-S@users.noreply.github.com>
2023-10-10 11:09:46 +0530

    Add files via upload

---
+++ ArithematicOperator.java
@@ -0,0 +1,33 @@
+package Operators;
+// +, -, *, /, %
+public class ArithematicOperator {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("addition of two numbers "+c);
+		
+		int d=20;
+		int e=10;
+		int f=d-e;
+		System.out.println("subtraction of two numbers "+f);
+		
+		int g=10;
+		int h=20;
+		int i=g*h;
+		System.out.println("multiplication of two numbers "+i);
+		
+		int j=10;
+		int k=2;
+		int l=j/k;
+		System.out.println("addition of two numbers "+l);
+		
+		int p=10;
+		int q=2;
+		int r=p%q;
+		System.out.println("addition of two numbers "+r);
+		
+		
+	}
+
+}
