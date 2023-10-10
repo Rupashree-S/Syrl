08643049bca7bc0547a55ebb7a2857253c7b1bed
Rupashree-S <147474468+Rupashree-S@users.noreply.github.com>
2023-10-10 11:10:30 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 10, b = 20;
+ a+=b;
+System.out.println(a);
+
+	}
+
+}
