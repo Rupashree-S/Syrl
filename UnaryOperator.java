c3b5c5f8890827bffdca04f58752b1a074164fb1
Rupashree-S <147474468+Rupashree-S@users.noreply.github.com>
2023-10-10 11:12:41 +0530

    Add files via upload

---
+++ UnaryOperator.java
@@ -0,0 +1,20 @@
+package Operators;
+
+public class UnaryOperator {
+
+	public static void main(String[] args) {
+		int x = 10;
+				int y=++x;
+		int b = 10;
+				int q=b++;
+		int c = 10;
+				int r=--c;
+		int d = 10;
+				int p=d--;
+				System.out.println(y);
+				System.out.println(q);
+				System.out.println(r);
+				System.out.println(p);
+	}
+
+}
