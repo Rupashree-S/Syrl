a704161e58b12508a6e49f61aef171e0222e764b
Rupashree-S <147474468+Rupashree-S@users.noreply.github.com>
2023-10-10 11:16:41 +0530

    Add files via upload

---
+++ Swapping_of_nos.java
@@ -0,0 +1,14 @@
+package Project;
+
+public class Swapping_of_nos {
+	public static void main(String[] args) {
+		int a = 50 ;
+		int b = 40;
+		int c = a;
+		a = b;
+		b = c;
+		System.out.println(a);
+	System.out.println(b);
+	}
+
+}
