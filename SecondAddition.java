81722f9aaf3940eae7cde3411907976285a6868b
Rupashree-S <147474468+Rupashree-S@users.noreply.github.com>
2023-10-10 11:14:26 +0530

    Add files via upload

---
+++ SecondAddition.java
@@ -0,0 +1,11 @@
+package com.edu;
+
+public class SecondAddition {
+	public static void main(String[] args) {
+		float a=10334567899466f;
+		int b=20;
+		float c=a+b;
+		System.out.println("values " +a+ " "+c);
+	}
+
+}
