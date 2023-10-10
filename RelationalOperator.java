2cb05c54dfb2ddb83da59df8581a77e1f1f8ad1a
Rupashree-S <147474468+Rupashree-S@users.noreply.github.com>
2023-10-10 11:12:04 +0530

    Add files via upload

---
+++ RelationalOperator.java
@@ -0,0 +1,18 @@
+package Operators;
+// >,<,>=,<=,!=,==
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+	int a=20;
+	int b=20;
+	//to check condition v use below statement
+	if(a == b) {
+		System.out.println("a is greater than b " +a);
+	}
+	else
+	{
+		System.out.println("b is greater than a " +b);
+	}
+
+}
+}
